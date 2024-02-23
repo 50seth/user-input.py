74bc9abf6203079363c94f574b2cdec1d32a9294
Seth Oketch <159660376+50seth@users.noreply.github.com>
2024-02-23 21:12:00 +0300

    Add files via upload

---
+++ user-input.py
@@ -0,0 +1,21 @@
+# user_input.py
+
+# Ask the user for their name and store it in the "name" variable
+name = input("SETH ODHIAMBO")
+
+# Ask the user for their age and store it in the "age" variable
+age = input("20")
+
+# Ask the user for their location and store it in the "location" variable
+location = input("NAIROBI: ")
+
+# Display the collected information
+print("\nUser Information:")
+print(f"Name: {SETH}")
+print(f"Age: {20}")
+print(f"Location: {NAIROBI}")
+
+# Display a personalized message using the collected information
+personalized_message = f"Hello {SETH}, you are {20} years old and live in {NAIROBI}"
+print("Congratulations for your PLP scholarship:")
+print(congragulatory-message)
